959a5a336020222402b5a808069d5bae56d85c6f
vitaly Krautsou <mr.rison2008@gmail.com>
2022-04-24 12:08:17 +0300

    qweqwe

--- hw_1js.js
+++ hw_1js.js
@@ -72,19 +72,19 @@ const age_3 = 60;
 //    }
 //    Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
-// const checkAge = function(age){
-//    if (age < age_2) {
-//        console.log("You don't have access cause your age is " + age + " It's less then " + age_2)
-//    } else if (age >= age_2 && age < age_3){
-//        console.log("Welcome!")
-//    } else if (age >= age_3){
-//        console.log("Keep calm and look Culture channel")
-//    } else
-//        console.log("Technical work")
-//    }
-// checkAge(17)
-// checkAge(18)
-// checkAge(61)
+const checkAge = function(age){
+   if (age < age_2) {
+       console.log("You don't have access cause your age is " + age + " It's less then " + age_2)
+   } else if (age >= age_2 && age < age_3){
+       console.log("Welcome!")
+   } else if (age >= age_3){
+       console.log("Keep calm and look Culture channel")
+   } else
+       console.log("Technical work")
+   }
+checkAge(17)
+checkAge(18)
+checkAge(61)
   
 //    2*:
 //    Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
